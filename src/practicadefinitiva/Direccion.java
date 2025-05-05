c310b407e9d4c72d73516fa2ca9f49cf39cd1f5b
Marco <Marco@PorttilMarco>
2025-05-05 17:42:53 +0200

    Segunda actualización

---
+++ src/practicadefinitiva/Direccion.java
@@ -0,0 +1,15 @@
+package practicadefinitiva;
+
+public class Direccion {
+    private String ciudad;
+    private String pais;
+
+    public Direccion(String ciudad, String pais) {
+        this.ciudad = ciudad;
+        this.pais = pais;
+    }
+
+    public String obtenerDireccionCompleta() {
+        return ciudad + ", " + pais;
+    }
+}
--- src/practicadefinitiva/Persona.java
+++ src/practicadefinitiva/Persona.java
@@ -3,14 +3,17 @@ package practicadefinitiva;
 public class Persona {
     private String nombre;
     private int edad;
+    private Direccion direccion;
 
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
+        this.direccion = new Direccion("Madrid", "España");
     }
 
     public void saludar() {
-        System.out.println("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
+        System.out.println("Hola, me llamo " + nombre + ", tengo " + edad + 
+                           " años y vivo en " + direccion.obtenerDireccionCompleta() + ".");
     }
 
     public void cumplirAnios() {
